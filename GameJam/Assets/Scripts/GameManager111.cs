28825320ec8ad8320715a68d5d2a7dbff4a98f77
Nikita Proskuryakov <tygovanderm@gmail.com>
2025-11-08 05:39:27 +0100

    GardenBed System + Basic Flowers +DefenseArea System + HelthBar

--- GameJam/Assets/Scripts/GameManager111.cs
+++ GameJam/Assets/Scripts/GameManager111.cs
@@ -1,36 +1,51 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
+
+[System.Serializable]
+public class FlowerItem
+{
+    public int FlowerID;
+    public string flowerName;
+    public GameObject flowerPrefab; 
+    public int price;               
+}
 
 public class GameManager111 : MonoBehaviour
 {
     [Header("Gameplay Stats")]
     [SerializeField]
-    private short moneyAmount;
-    [SerializeField]
-    private short gardenHP;
-    [SerializeField]
-    private short flowerPrice = 20;
+    private int playerGold;
+
 
     [Header("Gameobjects")]
-    private Text moneyText;
+    private Text playerGold_Text;
 
     [Header("Input Binds")]
     private string addMoneyKey = "AddMoney";
     private string spendMoneyKey = "SpendMoney";
+
+    [Header("Garden and Flowers")]
+    [SerializeField] private int gardenHP;
+    public List<FlowerItem> availableFlowers;
+
     void Start()
     {
-        if (moneyText == null)
+        if (playerGold_Text == null)
         {
-            moneyText = GameObject.Find("MoneyAmount")?.GetComponent<Text>();
+            playerGold_Text = GameObject.Find("MoneyAmount")?.GetComponent<Text>();
         }
 
-        if (moneyText != null)
-            moneyText.text = moneyAmount.ToString();
+        if (playerGold_Text != null)
+            playerGold_Text.text = playerGold.ToString();
         else
             Debug.LogError("Couldn't find 'MoneyAmount' Text object.");
     }   
     void Update()
     {
+        playerGold_Text.text = playerGold.ToString() ;
+
         if (Input.GetButtonDown(addMoneyKey))
         {
             AddMoney(100);
@@ -41,28 +56,49 @@ public class GameManager111 : MonoBehaviour
             SpentMoney(20);
         }
     }
-    private void AddMoney(short amount)
+    private void AddMoney(int amount)
     {
-        moneyAmount += amount;
+        playerGold += amount;
         UpdateMoneyUI();
     }
-    private void SpentMoney(short amount)
+    private void SpentMoney(int amount)
     {
-        if (moneyAmount >= amount)
+        if (playerGold >= amount)
         {
-            moneyAmount -= amount;
+            playerGold -= amount;
             UpdateMoneyUI();
         }
         else Debug.Log("You cant afford that !!!");
     }
     private void UpdateMoneyUI()
     {
-        if (moneyText != null)
-            moneyText.text = moneyAmount.ToString();
+        if (playerGold_Text != null)
+            playerGold_Text.text = playerGold.ToString();
     }
 
-    public void GetSpendMoneyForShop()
+
+    public void BuyFlower(int flowerID)
     {
-        SpentMoney(flowerPrice);
+        if (flowerID < 0 || flowerID >= availableFlowers.Count)
+        {
+            Debug.LogError("Неверный ID цветка!");
+            return;
+        }
+
+        FlowerItem item = availableFlowers[flowerID];
+
+        if (playerGold < item.price)
+        {
+            return;
+        }
+
+        if (!GardenManager.Instance.HasFreeBed())
+        {
+            return;
+        }
+
+        playerGold -= item.price;
+        GardenManager.Instance.PlantFlowerInFirstFreeBed(item.flowerPrefab);
     }
+
 }
